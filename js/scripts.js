b2970b77eadef754aec2d442c2e69b31f5ded21e
Joseph Escobedo <joe_escob1023@live.com>
2017-12-07 18:32:58 -0800

    add jquery and scroll event

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//allows user to click an animal type and scroll to the page with one click.
+$("a").on('click', function(event) {
+
+
+    if (this.hash !== "") {
+      
+      event.preventDefault();
+     
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+ 
+        window.location.hash = hash;
+      });
+    } 
+  });
